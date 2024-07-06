e4ab79ce128d5f4d92f568ef8a6485c0dc335468
Tushar Chandra <me@tusharc.dev>
2024-07-05 20:53:33 -0500

    fix: use non-relative paths for assets

---
+++ src/cup-utils.ts
@@ -0,0 +1,61 @@
+import { CupName } from './models';
+
+type CupConfig = {
+  id: CupName;
+  name: string;
+  maxCp: 1500 | 2500 | null;
+};
+
+function cupConfig(cup: CupName): CupConfig {
+  switch (cup) {
+    case CupName.great:
+      return {
+        id: CupName.great,
+        name: 'Great League',
+        maxCp: 1500,
+      };
+    case CupName.ultra:
+      return {
+        id: CupName.ultra,
+        name: 'Ultra League',
+        maxCp: 2500,
+      };
+    case CupName.master:
+      return {
+        id: CupName.master,
+        name: 'Master League',
+        maxCp: null,
+      };
+    case CupName.remix:
+      return {
+        id: CupName.remix,
+        name: 'Great League (Remix)',
+        maxCp: 1500,
+      };
+    case CupName.summer:
+      return {
+        id: CupName.summer,
+        name: 'Summer Cup',
+        maxCp: 1500,
+      };
+    case CupName.premiermaster:
+      return {
+        id: CupName.premiermaster,
+        name: 'Master League (Premier)',
+        maxCp: null,
+      };
+    case CupName.premierultra:
+      return {
+        id: CupName.premierultra,
+        name: 'Ultra League (Premier)',
+        maxCp: 2500,
+      };
+    default: {
+      const _never: never = cup;
+      throw new Error(`Unknown cup name: ${_never}`);
+    }
+  }
+}
+
+export { cupConfig };
+export type { CupConfig };
